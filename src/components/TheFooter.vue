e28b1189e19c083ece5fdf4d8297e266c6bfdba3
Juan Alvarez <127160252+JuanCarlosAT96@users.noreply.github.com>
2024-02-26 17:26:45 -0800

    Add files via upload

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer>
+    <span>TODOS LOS DERECHOS RESERVADOS</span>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+footer {
+  height: 532px;
+  background: var(--Accent, #1364ff);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+span {
+  color: white;
+}
+</style>
