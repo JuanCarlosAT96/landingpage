5257f4fa90c1a4a8a295fa759ccab54d84f5b93f
Juan Alvarez <127160252+JuanCarlosAT96@users.noreply.github.com>
2024-02-26 17:25:12 -0800

    Add files via upload

---
+++ landingpage/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <the-header></the-header>
+  <theHero></theHero>
+  <the-main></the-main>
+  <the-footer></the-footer>
+</template>
+
+<script lang="ts">
+import TheFooter from './components/TheFooter.vue';
+import TheHeader from "./components/TheHeader.vue";
+import TheHero from "./components/TheHero.vue";
+import TheMain from "./components/TheMain.vue";
+
+export default {
+  name: "App",
+  components: { TheHeader, TheHero, TheMain, TheFooter },
+};
+</script>
+
+<style></style>
